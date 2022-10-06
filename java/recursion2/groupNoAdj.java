6b8123231afec35f05c9b83438f6f129464e3308
marcos-aparicio <marcos.aparicio593@gmail.com>
2022-10-06 16:38:16 -0400

    groupNoAdj Recursion 2 Java Problem

---
+++ java/recursion2/groupNoAdj.java
@@ -0,0 +1,14 @@
+/*
+ *
+ * Given an array of ints, is it possible to choose a group of some of the ints, such that the group sums to the given target with this additional constraint: If a value in the array is chosen to be in the group, the value immediately following it in the array must not be chosen. (No loops needed.)
+ *
+ */
+public boolean groupNoAdj(int start, int[] nums, int target) {
+  
+  if(start >= nums.length) return target == 0; 
+
+  //to not consider the adjacent value
+  return groupNoAdj(start+2,nums,target-nums[start]) ||
+         groupNoAdj(start+1,nums,target);
+  
+}
