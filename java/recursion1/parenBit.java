b66fba14f101ecf43baf041720bd1dfe0e4c73aa
marcos-aparicio <marcos.aparicio593@gmail.com>
2022-10-18 07:48:56 -0400

    parenBit Recursion 1 Java Problem

---
+++ java/recursion1/parenBit.java
@@ -0,0 +1,21 @@
+/*
+ *
+Given a string that contains a single pair of parenthesis, compute recursively a new string made of only of the parenthesis and their contents, so "xyz(abc)123" yields "(abc)".
+
+
+parenBit("xyz(abc)123") → "(abc)"
+parenBit("x(hello)") → "(hello)"
+parenBit("(xy)1") → "(xy)"
+ *
+ *
+ */
+public String parenBit(String str) {
+  
+  if( str.contains("(") && str.charAt(0) != '(' ) return parenBit(str.substring(1));
+  
+  if( str.charAt(0) == ')' ) return ""+str.charAt(0);
+  
+
+  return str.charAt(0) + parenBit(str.substring(1));
+  
+}
